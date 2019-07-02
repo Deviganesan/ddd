9bcad9f87917d2e5cf0d144563a0216883c1d497
Deviganesan <51699366+Deviganesan@users.noreply.github.com>
2019-07-02 16:49:59 +0530

    Create max.c

---
+++ max.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+
+int arr[20],i,n;
+int large;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&arr[i]);
+}
+for(i=0;i<n;i++)
+{
+    large=arr[0];
+if(large<arr[i])
+    large=arr[i];
+}
+printf("%d",large);
+
+}
